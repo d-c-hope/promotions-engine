8dc2369faadc59098995173e37601d8f60a5edc8
davidhope <david_hope2@me.com>
2020-01-28 21:36:19 +0000

    More producer events

--- event_producer/producer.py
+++ event_producer/producer.py
@@ -2,6 +2,7 @@ from confluent_kafka import Producer
 
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
+import random
 
 
 value_schema_str = """
@@ -46,15 +47,28 @@ key_schema_str = """
 
 value_schema = avro.loads(value_schema_str)
 key_schema = avro.loads(key_schema_str)
-value = {"game": "randomgame", "action": "action 4", "customerID": "23416", "stake": 23 }
-key = {"customerID": "23416"}
 
 avroProducer = AvroProducer({
     'bootstrap.servers': 'localhost:9092',
     'schema.registry.url': 'http://localhost:8081'
     }, default_key_schema=key_schema, default_value_schema=value_schema)
 
-for i in range(2):
+for i in range(200):
+
+    customerId = random.randint(1, 10)¶
+    action = random.randint(1, 4)¶
+    stake = random.randint(50, 100)¶
+
+
+    # value = {"game": "randomgame", "action": "action 4", "customerID": "{}".format(customerId), "stake": 23 }
+    # key = {"customerID": "23416"}
+
+    value = {"game": "randomgame", "action": "action {}".format(action),
+             "customerID": "{}".format(customerId), "stake": stake}
+    key = {"customerID": customerId}
+
+
     print("adding event")
     avroProducer.produce(topic='test-topic-1', value=value, key=key)
+    time.sleep(0.5)
 avroProducer.flush()
