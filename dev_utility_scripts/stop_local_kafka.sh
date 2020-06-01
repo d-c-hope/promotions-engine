e2f5a77e49739cee94d1001631266c7112165042
dho15 <david_hope2@me.com>
2020-06-01 08:57:39 +0100

    Stop Kafka script had ordering wrong

--- dev_utility_scripts/stop_local_kafka.sh
+++ dev_utility_scripts/stop_local_kafka.sh
@@ -1,5 +1,5 @@
 pushd ${MY_KAFKA}
-./bin/zookeeper-server-stop.sh config/zookeeper.properties
-sleep 5s
 ./bin/kafka-server-stop.sh config/server.properties
+sleep 5s
+./bin/zookeeper-server-stop.sh config/zookeeper.properties
 popd
